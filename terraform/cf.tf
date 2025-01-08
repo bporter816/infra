3f090287e5e7a3febddc73c11cbc71b458a80ecb
Benjamin Porter <bporter816@gmail.com>
2025-01-08 00:33:48 -0500

    Add website infra

---
+++ terraform/cf.tf
@@ -0,0 +1,105 @@
+data "aws_secretsmanager_secret_version" "website_secrets" {
+  secret_id = "website-secrets"
+}
+
+locals {
+  website_secrets = jsondecode(data.aws_secretsmanager_secret_version.website_secrets.secret_string)
+}
+
+resource "cloudflare_zone" "benjaminporter_me" {
+  account_id = var.cloudflare_account_id
+  zone       = "benjaminporter.me"
+  type       = "full"
+  plan       = "free"
+}
+
+resource "cloudflare_pages_project" "benjaminporter_me" {
+  account_id        = var.cloudflare_account_id
+  production_branch = "main"
+  name              = "benjaminporter-me"
+
+  source {
+    type = "github"
+    config {
+      owner             = "bporter816"
+      repo_name         = "benjaminporter.me"
+      production_branch = "main"
+    }
+  }
+
+  build_config {
+    build_command   = "sh build.sh"
+    destination_dir = "public"
+    build_caching   = false
+  }
+
+  deployment_configs {
+    production {
+      environment_variables = {
+        SPOTIFY_COUNT    = 3
+        SPOTIFY_ENDPOINT = "playlist"
+        STEAM_COUNT      = 3
+
+      }
+      secrets = {
+        SPOTIFY_CLIENT_ID     = local.website_secrets["SPOTIFY_CLIENT_ID"]
+        SPOTIFY_CLIENT_SECRET = local.website_secrets["SPOTIFY_CLIENT_SECRET"]
+        SPOTIFY_REFRESH_TOKEN = local.website_secrets["SPOTIFY_REFRESH_TOKEN"]
+        SPOTIFY_PLAYLIST      = local.website_secrets["SPOTIFY_PLAYLIST"]
+        STEAM_USER_ID         = local.website_secrets["STEAM_USER_ID"]
+        STEAM_API_KEY         = local.website_secrets["STEAM_API_KEY"]
+      }
+      fail_open   = true
+      usage_model = "standard"
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "benjaminporter_me_domain" {
+  account_id   = var.cloudflare_account_id
+  project_name = "benjaminporter-me"
+  domain       = "benjaminporter.me"
+}
+
+resource "cloudflare_record" "apex" {
+  zone_id = cloudflare_zone.benjaminporter_me.id
+  type    = "CNAME"
+  name    = "@"
+  content = "benjaminporter-me.pages.dev"
+  proxied = true
+}
+
+resource "cloudflare_workers_script" "rebuild" {
+  account_id = var.cloudflare_account_id
+  name       = "rebuild-site"
+  module     = true
+  content    = <<-EOT
+    async function rebuild(env) {
+      await fetch(env.DEPLOY_HOOK_URL,
+        {
+          method: "POST",
+        },
+      )
+    }
+    export default {
+      async scheduled(event, env, ctx) {
+        ctx.waitUntil(rebuild(env));
+      },
+    };
+  EOT
+}
+
+resource "cloudflare_workers_cron_trigger" "rebuild_trigger" {
+  account_id  = var.cloudflare_account_id
+  script_name = cloudflare_workers_script.rebuild.name
+  schedules = [
+    "0 0 * * *", # daily at midnight
+  ]
+}
+
+resource "cloudflare_workers_secret" "deploy_hook_url" {
+  account_id  = var.cloudflare_account_id
+  name        = "DEPLOY_HOOK_URL"
+  script_name = cloudflare_workers_script.rebuild.name
+  secret_text = local.website_secrets["DEPLOY_HOOK_URL"]
+}
---
+++ terraform/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.10.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.82.2"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.49.1"
+    }
+  }
+}
