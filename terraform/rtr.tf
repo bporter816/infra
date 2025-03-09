cfd1bb7b22a8e014efbbe8a1725e7ad4f79dcde5
Benjamin Porter <bporter816@gmail.com>
2025-03-09 13:06:30 -0400

    Add transiter LB record

---
+++ terraform/rtr.tf
@@ -0,0 +1,6 @@
+resource "cloudflare_record" "transiter" {
+  zone_id = module.benjaminporter_me.cloudflare_zone_id
+  type    = "CNAME"
+  name    = "transiter.benjaminporter.me"
+  content = "transiter-1540602530.us-east-1.elb.amazonaws.com"
+}
