e977ad7f7a923dee334aace1191ed2fc219fec1d
Benjamin Porter <bporter816@gmail.com>
2025-03-09 13:20:27 -0400

    Fix cert common name

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -5,6 +5,6 @@ resource "aws_ecs_cluster" "main" {
 resource "cloudflare_record" "rtr_validation" {
   zone_id = module.benjaminporter_me.cloudflare_zone_id
   type    = "CNAME"
-  name    = "_ff50614463ea5991afcb3199f9efc2b6.benjaminporter.me"
-  content = "_b44f2b810192bb92d37cfa345d4786ef.zzssrbpcnq.acm-validations.aws."
+  name    = "_8875065ac9745f376bbeb44a347d034b.transiter.benjaminporter.me."
+  content = "_339915cc69d3e726030f9b061a614980.xlfgrmvvlj.acm-validations.aws."
 }
