c0cf5ef6ec79a715c55cd6d82fbf92ed56a600d3
Benjamin Porter <bporter816@gmail.com>
2025-04-14 00:17:19 -0500

    Remove ACM validation record

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,10 +1,3 @@
 resource "aws_ecs_cluster" "main" {
   name = "main"
 }
-
-resource "cloudflare_record" "rtr_validation" {
-  zone_id = module.benjaminporter_me.cloudflare_zone_id
-  type    = "CNAME"
-  name    = "_8875065ac9745f376bbeb44a347d034b.transiter.benjaminporter.me."
-  content = "_339915cc69d3e726030f9b061a614980.xlfgrmvvlj.acm-validations.aws."
-}
