01612f9335ee193c17b2d8d2736f977f84b8cea2
Benjamin Porter <bporter816@gmail.com>
2025-04-14 00:16:53 -0500

    Upgrade Terraform and AWS provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,15 +5,19 @@ terraform {
       name = "infra"
     }
   }
-  required_version = "~> 1.10.4"
+  required_version = "~> 1.11.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.86.1"
+      version = "~> 5.94.1"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.52.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
     }
   }
 }
